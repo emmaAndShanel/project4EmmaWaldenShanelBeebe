7089d8696cc6fb0253cd554c632d7c802e80151c
Emma Walden <emmajanewalden@gmail.com>
2020-04-13 18:00:57 -0400

    pseudo code and namespace for app

---
+++ app.js
@@ -0,0 +1,33 @@
+// An app that generates nature images for users based on their selection from a dropdown of options
+// A landing page with title and message about how to use the app
+// A dropdown menu with nature search terms 
+// A background where the images will be displayed below the dropdown
+// Create app namespace to hold all methods
+// Add event listener to collect the user input from the dropdown 
+// Save user input in a variable 
+// Use that variable to make AJAX request 
+// Display related images on the page in a gallery 
+// When user clicks on an image it scales up in size (modal)
+
+// Stretch goals
+// Create a carousel on the modal of the images that users can click through 
+// Add soothing nature sounds 
+
+
+const app = {};
+
+// Collect user input
+app.collectInfo = function () { };
+
+// Make AJAX request with user inputted data
+app.getInfo = function () { };
+
+// Display data on the page
+app.displayInfo = function () { };
+
+// Start app
+app.init = function () { };
+
+$(function () {
+    app.init();
+});
