6b213b60ea3df49b03b85cd4dfab638efd6e2703
Shanel Beebe <shanelbeebe@icloud.com>
2020-04-13 19:52:45 -0400

    first ajax request

--- app.js
+++ app.js
@@ -1,33 +1,45 @@
 // An app that generates nature images for users based on their selection from a dropdown of options
 // A landing page with title and message about how to use the app
-// A dropdown menu with nature search terms 
+// A dropdown menu with nature search terms
 // A background where the images will be displayed below the dropdown
 // Create app namespace to hold all methods
-// Add event listener to collect the user input from the dropdown 
-// Save user input in a variable 
-// Use that variable to make AJAX request 
-// Display related images on the page in a gallery 
+// Add event listener to collect the user input from the dropdown
+// Save user input in a variable
+// Use that variable to make AJAX request
+// Display related images on the page in a gallery
 // When user clicks on an image it scales up in size (modal)
 
 // Stretch goals
-// Create a carousel on the modal of the images that users can click through 
-// Add soothing nature sounds 
-
+// Create a carousel on the modal of the images that users can click through
+// Add soothing nature sounds
 
 const app = {};
 
 // Collect user input
-app.collectInfo = function () { };
+app.collectInfo = function () {};
 
 // Make AJAX request with user inputted data
-app.getInfo = function () { };
+app.getInfo = function () {
+  $.ajax({
+    url: "https://api.pexels.com/v1/search?query=nature&per_page=15&page=1",
+    method: "GET",
+    dataType: "json",
+    headers: {
+      authorization: "563492ad6f917000010000018090d1171f7a46398e0de7e0ada31b47",
+    },
+  }).then((result) => {
+    console.log(result);
+  });
+};
 
 // Display data on the page
-app.displayInfo = function () { };
+app.displayInfo = function () {};
 
 // Start app
-app.init = function () { };
+app.init = function () {
+  app.getInfo();
+};
 
 $(function () {
-    app.init();
+  app.init();
 });
