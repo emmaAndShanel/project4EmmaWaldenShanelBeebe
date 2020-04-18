c22c6f14f34a08b5cdc4ca613ccf5a770f46751e
Emma Walden <emmajanewalden@gmail.com>
2020-04-18 17:46:13 -0400

    added favicon, sound off img, changed sound object to add loop, we reworked the getSound function to remove the option on click event listener so that it will work in Chrome

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 const app = {};
 
-app.soundObject = {
-  autumn: '/styles/assets/sounds/autumn.mp3',
-  blossoms: '/styles/assets/sounds/blossoms.mp3',
-  canyon: '/styles/assets/sounds/canyon.mp3',
-  forest: '/styles/assets/sounds/forest.mp3',
-  lights: '/styles/assets/sounds/lights.mp3',
-  rainforest: '/styles/assets/sounds/rainforest.mp3',
-  sky: '/styles/assets/sounds/sky.mp3',
-  stream: '/styles/assets/sounds/stream.mp3',
-  waves: '/styles/assets/sounds/waves.mp3',
-}
+
+app.soundObject = {}
+
+app.soundObject.autumn = new Audio("/styles/assets/sounds/autumn.mp3")
+app.soundObject.autumn.loop = true;
+app.soundObject.blossoms = new Audio("/styles/assets/sounds/blossoms.mp3")
+app.soundObject.blossoms.loop = true;
+app.soundObject.canyon = new Audio("/styles/assets/sounds/canyon.mp3")
+app.soundObject.canyon.loop = true;
+app.soundObject.forest = new Audio("/styles/assets/sounds/forest.mp3")
+app.soundObject.forest.loop = true;
+app.soundObject.lights = new Audio("/styles/assets/sounds/lights.mp3")
+app.soundObject.lights.loop = true;
+app.soundObject.rainforest = new Audio("/styles/assets/sounds/rainforest.mp3")
+app.soundObject.rainforest.loop = true;
+app.soundObject.sky = new Audio("/styles/assets/sounds/sky.mp3")
+app.soundObject.sky.loop = true;
+app.soundObject.stream = new Audio("/styles/assets/sounds/stream.mp3")
+app.soundObject.stream.loop = true;
+app.soundObject.waves = new Audio("/styles/assets/sounds/waves.mp3")
+app.soundObject.waves.loop = true;
 
 // Collect user input and store in a variable
 app.getData = function () {
@@ -59,20 +69,24 @@ app.displayInfo = function (data) {
   });
 };
 
-
 app.getSound = function () {
-  $("option").on("click", function (e) {
-    const userId = $(this).attr('id');
-  for (let sound in app.soundObject) {
-    const soundFile = app.soundObject[sound];
-    if (userId === sound) {
-      $('audio').html(`<source src="${soundFile}"><source>`);
-      const audio = $('audio')
-      audio[0].play();
-      $('audio').prop('volume', 0.1)
+  $("select").on("change", function () {
+    const userId = $(this).children(":selected").attr("id");
+    for (let sound in app.soundObject) {
+      const soundFile = app.soundObject[sound];
+      if (userId === sound) {
+        $(".audio").html(soundFile);
+        const audio = $("audio")
+        audio[0].play();
+        $("audio").prop("volume", 0.1)
+        $(".soundButton").html(`
+        <button><img class="soundOff" src="./styles/assets/soundOffsvg.svg"/></button>`)
+        $(".soundOff").on("click", function () {
+          audio[0].pause();
+        })
+      }
     }
-  }
-})
+  })
 }
 
 
@@ -104,7 +118,7 @@ app.handleKeyUp = (e) => {
 app.modalImage = () => {
   $(".returnedImage").on("click", function (e) {
     $(".modalContent").html(
-        `<figure><img src="${e.currentTarget.src}" alt="${e.currentTarget.alt}"/>
+      `<figure><img src="${e.currentTarget.src}" alt="${e.currentTarget.alt}"/>
         <span class="close"><img src="./styles/assets/close.svg"/></span></figure>`
     );
     app.openModal();
